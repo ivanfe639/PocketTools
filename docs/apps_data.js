bc72a6c3f42bce95278f45582330ee7e2aa0e345
Alvaro Andres Obregon Carreño <alvaroa639@gmail.com>
2026-02-11 10:44:36 -0500

    control gasolina

--- docs/apps_data.js
+++ docs/apps_data.js
@@ -1,13 +1,18 @@
 ﻿// This file is auto-generated. Do not edit manually.
 const APPS_DATA = [
     {
-        "name":  "Control de Gastos | Premium Management",
-        "path":  "controlGastos.html",
-        "filename":  "controlGastos.html"
+        "name": "Control de Gasolina | Premium Management",
+        "path": "controlgasolina.html",
+        "filename": "controlgasolina.html"
     },
     {
-        "name":  "Lista de Tareas con Estadísticas",
-        "path":  "lista_tareas_estadisticas.html",
-        "filename":  "lista_tareas_estadisticas.html"
+        "name": "Control de Gastos | Premium Management",
+        "path": "controlGastos.html",
+        "filename": "controlGastos.html"
+    },
+    {
+        "name": "Lista de Tareas con Estadísticas",
+        "path": "lista_tareas_estadisticas.html",
+        "filename": "lista_tareas_estadisticas.html"
     }
 ];
