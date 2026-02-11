3dd2fcd8ced51a4b8399161708edb34fcea6c618
Alvaro Andres Obregon Carreño <alvaroa639@gmail.com>
2026-02-11 11:02:46 -0500

    Mejora en el proceso de seleccion de icono

--- docs/apps_data.js
+++ docs/apps_data.js
@@ -1,18 +1,21 @@
 ﻿// This file is auto-generated. Do not edit manually.
 const APPS_DATA = [
     {
-        "name": "Control de Gasolina | Premium Management",
-        "path": "controlgasolina.html",
-        "filename": "controlgasolina.html"
+        "name":  "Control de Gasolina | Premium Management",
+        "path":  "controlgasolina.html",
+        "filename":  "controlgasolina.html",
+        "icon":  "fuel"
     },
     {
-        "name": "Control de Gastos | Premium Management",
-        "path": "controlGastos.html",
-        "filename": "controlGastos.html"
+        "name":  "Control de Gastos | Premium Management",
+        "path":  "controlGastos.html",
+        "filename":  "controlGastos.html",
+        "icon":  "wallet"
     },
     {
-        "name": "Lista de Tareas con Estadísticas",
-        "path": "lista_tareas_estadisticas.html",
-        "filename": "lista_tareas_estadisticas.html"
+        "name":  "Lista de Tareas con Estadísticas",
+        "path":  "lista_tareas_estadisticas.html",
+        "filename":  "lista_tareas_estadisticas.html",
+        "icon":  "list-todo"
     }
 ];
